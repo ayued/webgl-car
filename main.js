3b36524cde0ce79cd396a8e55ca8595dc39c0291
ayumi <>
2025-02-12 01:02:10 +0900

    fix cdn  version

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 import * as THREE from "three";
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 import { OrbitControls } from "three/addons/controls/OrbitControls.js";
-import gsap from "/node_modules/gsap/index.js";
 
 const width = window.innerWidth-60;
 const height = window.innerHeight-200;
