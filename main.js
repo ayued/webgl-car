132fb7bc3b3a77aec6a651ec5565d20e075a4918
ayumi <>
2025-02-12 00:52:14 +0900

    fix

--- main.js
+++ main.js
@@ -1,8 +1,7 @@
 import * as THREE from "three";
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 import { OrbitControls } from "three/addons/controls/OrbitControls.js";
-// import gsap from "/node_modules/gsap/index.js";
-import { gsap } from "https://cdnjs.cloudflare.com/ajax/libs/gsap/3.12.7/gsap.min.js";
+import gsap from "/node_modules/gsap/index.js";
 
 const width = window.innerWidth-60;
 const height = window.innerHeight-200;
